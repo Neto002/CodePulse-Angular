e13a97d8d587792adf4010515aaf89f3d872cfce
Antonio Neto <antoniogfneto92@hotmail.com>
2024-05-02 18:10:45 -0300

    filtering, sorting and pagination

--- src/app/features/category/category-list/category-list.component.ts
+++ src/app/features/category/category-list/category-list.component.ts
@@ -1,8 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {Category} from "../models/category.model";
-import {Router} from "@angular/router";
-import {CategoryService} from "../services/category.service";
-import {Observable} from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { Category } from "../models/category.model";
+import { CategoryService } from "../services/category.service";
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-category-list',
@@ -12,14 +11,72 @@ import {Observable} from "rxjs";
 export class CategoryListComponent implements OnInit {
 
   categories$?: Observable<Category[]>;
+  totalCount?: number;
+  list: number[] = []
+  query: string = '';
+  pageNumber: number = 1;
+  pageSize: number = 3;
 
-  constructor(private categoryService: CategoryService) {}
-
-  ngOnInit(): void {
-    this.getAllCategories();
+  constructor(private categoryService: CategoryService) {
   }
 
-  getAllCategories() {
-    this.categories$ = this.categoryService.getAllCategories();
+  ngOnInit(): void {
+    this.categoryService.getCategoryCount().subscribe({
+      next: (count) => {
+        this.totalCount = count;
+        this.list = new Array(Math.ceil(count / this.pageSize));
+
+        this.getAllCategories();
+      }
+    })
+
+  }
+
+  getAllCategories(pageNumber?: number) {
+    this.categories$ = this.categoryService.getAllCategories(
+      undefined,
+      undefined,
+      undefined,
+      pageNumber ?? this.pageNumber,
+      this.pageSize
+    );
+  }
+
+  onSearch() {
+    this.categories$ = this.categoryService.getAllCategories(this.query);
+  }
+
+  sort(sortBy: string, sortDirection: string, pageNumber?: number, pageSize?: number) {
+    console.log(this.pageNumber)
+    this.categories$ = this.categoryService.getAllCategories(
+      this.query,
+      sortBy,
+      sortDirection,
+      pageNumber ?? this.pageNumber,
+      pageSize ?? this.pageSize
+    );
+  }
+
+  getPage(pageNumber: number) {
+    this.pageNumber = pageNumber;
+    this.getAllCategories(pageNumber);
+  }
+
+  getPrevPage() {
+    if (this.pageNumber - 1 < 1) {
+      return;
+    }
+
+    this.pageNumber--;
+    this.getPage(this.pageNumber);
+  }
+
+  getNextPage() {
+    if (this.pageNumber + 1 > this.list.length) {
+      return;
+    }
+
+    this.pageNumber++;
+    this.getPage(this.pageNumber);
   }
 }
--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from "../models/add-category-request.model";
 import { Observable } from "rxjs";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Category } from "../models/category.model";
 import { environment } from "../../../../environments/environment";
 import { UpdateCategoryRequest } from "../models/update-category-request.model";
@@ -17,12 +17,40 @@ export class CategoryService {
               private cookieService: CookieService,) {
   }
 
-  getAllCategories(): Observable<Category[]> {
-    return this.http.get<Category[]>(this.categoryEndpointUrl);
+  getAllCategories(query?: string, sortBy?: string, sortDirection?: string, pageNumber?: number, pageSize?: number): Observable<Category[]> {
+    let params: HttpParams = new HttpParams();
+
+    if (query) {
+      params = params.set('query', query);
+    }
+
+    if (sortBy) {
+      params = params.set('sortBy', sortBy);
+    }
+
+    if (sortDirection) {
+      params = params.set('sortDirection', sortDirection);
+    }
+
+    if (pageNumber) {
+      params = params.set('pageNumber', pageNumber);
+    }
+
+    if (pageSize) {
+      params = params.set('pageSize', pageSize);
+    }
+
+    return this.http.get<Category[]>(this.categoryEndpointUrl, {
+      params: params
+    });
   }
 
   getCategoryById(id: string): Observable<Category> {
     return this.http.get<Category>(`${this.categoryEndpointUrl}/${id}`);
+  }
+
+  getCategoryCount(): Observable<number> {
+    return this.http.get<number>(`${this.categoryEndpointUrl}/count`);
   }
 
   addCategory(model: AddCategoryRequest): Observable<void> {
